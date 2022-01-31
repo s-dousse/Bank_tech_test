85ed08c3000d6f70989f67f5b49fe25355d7f0bf
Sarah <dousse.sarah@gmail.com>
2022-01-31 23:14:40 +0000

    refactoring: merging #save_debit and #save_credit into #save_statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,24 +1,26 @@
 class Statement
-  def save_debit(amount, date, balance)
-    statement = {
-      :date => date,
-      :credit => "-",
-      :debit => format_string(amount),
-      :balance => format_string(balance)
-    }
-  end
 
-  def save_credit(amount, date, balance)
+  def save_statement(amount, date, balance, status)
     statement = {
       :date => date,
-      :credit => format_string(amount),
-      :debit => "-",
       :balance => format_string(balance)
     }
+    if is_a_debit?(status)
+      statement.merge!(:debit => format_string(amount))
+      statement.merge!(:credit => "-")
+    else
+      statement.merge!(:debit => "-")
+      statement.merge!(:credit => format_string(amount))
+    end
+    statement
   end
 
   private
   def format_string(num)
     sprintf('%.2f', num)
+  end
+
+  def is_a_debit?(status)
+    status == true
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -5,12 +5,12 @@ describe Statement do
     let!(:statement) { Statement.new }
 
     it "creates a debit statement when a withdrawal is made" do
-      debit_statement = statement.save_debit(100, "01/02/22", 1500)
+      debit_statement = statement.save_statement(100, "01/02/22", 1500, true)
       expect(debit_statement).to include(:date => "01/02/22", :debit => "100.00", :credit => "-", :balance => "1500.00")
     end
 
     it "creates a credit statement when a deposit is made" do
-      credit_statement = statement.save_credit(150, "02/02/22", 2000)
+      credit_statement = statement.save_statement(150, "02/02/22", 2000, false)
       expect(credit_statement).to include(:date => "02/02/22", :debit => "-", :credit => "150.00", :balance => "2000.00")
     end
   end
