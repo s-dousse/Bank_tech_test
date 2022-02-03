2d3bb9cdd0cfcad4ba2751279692b8e27332b028
Sarah <dousse.sarah@gmail.com>
2022-02-03 10:54:19 +0000

    refactoring all tests using eq

--- spec/bankAccount_spec.rb
+++ spec/bankAccount_spec.rb
@@ -8,18 +8,18 @@ describe BankAccount do
 
     context 'the account has sufficient funds' do
       it 'has a default balance of 0 when opened' do
-        expect(account.print_balance).to include '0.00'
+        expect(account.print_balance).to eq('The current balance is 0.00')
       end
 
       it 'can receive money' do
         account.deposit(50.00, '01/01/22')
-        expect(account.print_balance).to include '50.00'
+        expect(account.print_balance).to eq('The current balance is 50.00')
       end
 
       it 'can withdraw money' do
         account.deposit(50.00, '01/01/22')
         account.withdraw(30.00, '02/01/22')
-        expect(account.print_balance).to include '20.00'
+        expect(account.print_balance).to eq('The current balance is 20.00')
       end
     end
 
@@ -37,24 +37,24 @@ describe BankAccount do
 
     context 'the account has sufficient funds' do
       it 'has no statement when just opened' do
-        expect(account.print_statements.length).to be(1)
-        expect(account.print_statements).to include('date || credit || debit || balance')
+        expect(account.print_statements.length).to eq(1)
+        expect(account.print_statements[0]).to eq('date || credit || debit || balance')
       end
 
       it 'has 1 credit statement after a deposit' do
         account.statements.unshift(double_debit_st)
         expect(account.print_statements.length).to be(2)
-        expect(account.print_statements[0]).to include('date || credit || debit || balance')
-        expect(account.print_statements[1]).to include('01/01/22 || 50.00 || - || 50.00')
+        expect(account.print_statements[0]).to eq('date || credit || debit || balance')
+        expect(account.print_statements[1]).to eq('01/01/22 || 50.00 || - || 50.00')
       end
 
       it 'has 1 debit statement after a withdral' do
         account.statements.unshift(double_debit_st)
         account.statements.unshift(double_credit_st)
-        expect(account.print_statements.length).to be(3)
-        expect(account.print_statements[0]).to include('date || credit || debit || balance')
-        expect(account.print_statements[1]).to include('02/01/22 || - || 30.00 || 20.00')
-        expect(account.print_statements[2]).to include('01/01/22 || 50.00 || - || 50.00')
+        expect(account.print_statements.length).to eq(3)
+        expect(account.print_statements[0]).to eq('date || credit || debit || balance')
+        expect(account.print_statements[1]).to eq('02/01/22 || - || 30.00 || 20.00')
+        expect(account.print_statements[2]).to eq('01/01/22 || 50.00 || - || 50.00')
       end
     end
 
@@ -62,14 +62,14 @@ describe BankAccount do
       it 'creates no statement - withdraw from an empty account' do
         expect { account.withdraw(30.00, '02/01/22') }.to raise_error 'Sorry, your balance is insufficient'
 
-        expect(account.print_statements.length).to be(1)
+        expect(account.print_statements.length).to eq(1)
       end
 
       it 'creates no statement - insufficient funds' do
         account.deposit(50.00, '01/01/22')
         expect { account.withdraw(60.00, '02/01/22') }.to raise_error 'Sorry, your balance is insufficient'
-        expect(account.print_balance).to include '50.00'
-        expect(account.statements.length).to be(1)
+        expect(account.print_balance).to eq('The current balance is 50.00')
+        expect(account.statements.length).to eq(1)
       end
     end
   end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,12 +8,12 @@ describe Statement do
 
     it 'creates a debit statement when a withdrawal is made' do
       debit_statement = statement.save_statement(100, '01/02/22', 1500, true)
-      expect(debit_statement).to include(date: '01/02/22', debit: '100.00', credit: '-', balance: '1500.00')
+      expect(debit_statement).to eq(date: '01/02/22', debit: '100.00', credit: '-', balance: '1500.00')
     end
 
     it 'creates a credit statement when a deposit is made' do
       credit_statement = statement.save_statement(150, '02/02/22', 2000, false)
-      expect(credit_statement).to include(date: '02/02/22', debit: '-', credit: '150.00', balance: '2000.00')
+      expect(credit_statement).to eq(date: '02/02/22', debit: '-', credit: '150.00', balance: '2000.00')
     end
   end
 end
